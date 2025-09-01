23d5b69607db7db0006efc35ce27107b9a1215ed
JunYoungJee <rainmaker8492@gmail.com>
2025-09-01 16:46:08 +0900

    웹 뻑나는 것 다시 수정 요청하고 커밋

--- vite.config.ts
+++ vite.config.ts
@@ -15,4 +15,11 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "sass:math"; @import "@/app/styles/_variables.scss";`
+      }
+    }
+  }
 })
